ba28e2cd924b44d10e11064af5c4da4bc52ad2ea
Shekhar Upadhaya <shekhar_8888@hotmail.com>
2015-06-12 14:09:30 +0530

    Update merge.py
    Added comments and doc string statements.

--- Hackerrank/merge.py
+++ Hackerrank/merge.py
@@ -1,54 +1,64 @@
-def check_merge(j):
-	if merge_check_dict[j] == 0:
-		merge_check_dict[j] = 1
-		for item in xrange(j+1):
-			merge_check_dict[item] = 1
-		return 1
-	else:
-		return 0
-def merge(line):
-	result = [0]*len(line)
-	for i in xrange(len(line)):
-		merge_check_dict[i] = 0
+"""
+This is a docstring for the module:
 
-	for i,item in enumerate(line):
-		if item != 0:
-			# print 'item',item, i
-			if i == 0:
-				result[i] = item
-				# print result
-			else:
-				set_flag = 0
-				for j in xrange(i,0,-1):
-					# print i,j-1
-					if result[j-1] == 0:
-						# print 'case 1'
-						result[j-1] = item
-						result[j] = 0
-						set_flag=1
-						# print result
-					elif result[j-1] == item and check_merge(j-1):
-						# print 'Before merging',result
-						# print 'case 2-merging',i,j-1
-						result[j-1] = item + item
-						result[j] = 0
-						# print 'After merging',result
-						break
-					elif result[j-1] != item and set_flag == 1:
-						# print 'case 3'
-						# print result
-						break
-					elif result[j-1] != item and set_flag == 0:
-						# print 'case 4'
-						result[i] = item
-						# print result
-						break
-			# merge_check_dict[i] = 1
-	return result
+This module demonstrates the merge functionality
+in the 2048 game.
+"""
+def check_merge(j):
+    """
+    Check whether the merge has happen at a particular
+    index or not, and returns the appropriate boolean value
+    for the same.
+    """
+    if merge_check_dict[j] == 0:
+        merge_check_dict[j] = 1
+        for item in xrange(j+1):
+            merge_check_dict[item] = 1
+        return 1
+    else:
+        return 0
+def merge(line):
+    """
+    Return a new merge list for the given input list.
+    """
+    # Start with a result list that contains the same number of 0's as the length of the line argument. 
+    result = [0]*len(line)
+    # Initialize the dictionary with default value 0
+    for index in xrange(len(line)):
+        merge_check_dict[index] = 0
+    
+    # Iterate over every value in the list
+    for index,item in enumerate(line):
+    	# Iterate over the line input looking for non-zero entries
+        if item != 0:
+        	# Put the first index element directly in the result list
+            if index == 0:
+                result[index] = item
+                # For the rest of the elements, iterate towards the front of the list and start merging
+            else:
+                set_flag = 0
+                for j in xrange(index,0,-1):
+               	    # For 0 elements
+                    if result[j-1] == 0:
+                        result[j-1] = item
+                        result[j] = 0
+                        set_flag=1
+                    # For elements which are same and merge can be applied
+                    elif result[j-1] == item and check_merge(j-1):
+                        result[j-1] = item + item
+                        result[j] = 0
+                        break
+                    elif result[j-1] != item and set_flag == 1:
+                        break
+                    elif result[j-1] != item and set_flag == 0:
+                        result[index] = item
+                        break
+
+    return result
 
 # lst1 = map(int,raw_input().split())
 lst1 = [8,8]
+# Initialize an empty dictionary
 merge_check_dict = {}
-lst2 = merge(lst1)
-# print lst1            # You should get [2, 0, 2, 4]
-print lst2            # You should get [4, 4, 0, 0]
+# Printing the result
+print merge(lst1)
